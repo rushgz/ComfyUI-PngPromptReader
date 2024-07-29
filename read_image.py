6070efe690aba73d086c60e49ebbc0e6200ab8d0
rushgz <d122436@126.com>
2024-07-29 16:33:35 +0800

    添加读取输入图片转PIL

--- read_image.py
+++ read_image.py
@@ -1,5 +1,6 @@
 from PIL import Image
-
+import folder_paths
+import os
 
 class ReadImage:
 
@@ -27,14 +28,43 @@ class ReadImage:
         image = Image.open(text)
         return (image,)
 
+class LoadImagePIL:
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(s):
+        input_dir = folder_paths.get_input_directory()
+        files = [f for f in os.listdir(input_dir) if os.path.isfile(os.path.join(input_dir, f))]
+        return {"required":
+                    {"image": (sorted(files), {"image_upload": True})},
+                }
+
+    CATEGORY = "image"
+
+    RETURN_TYPES = ("PIL",)
+    RETURN_NAMES = ("image",)
+
+    FUNCTION = "read_image"
+
+    # OUTPUT_NODE = False
+
+    def read_image(self, image):
+        image_path = folder_paths.get_annotated_filepath(image)
+        image = Image.open(image_path)
+        return (image,)
+
 
 # A dictionary that contains all nodes you want to export with their names
 # NOTE: names should be globally unique
 NODE_CLASS_MAPPINGS = {
-    "ReadImage": ReadImage
+    "ReadImage": ReadImage,
+    "LoadImagePIL": LoadImagePIL
 }
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "ReadImage": "Read image to pil"
+    "ReadImage": "Read image to pil",
+    "LoadImagePIL": "Read image to pil"
 }
